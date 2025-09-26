597ab75193316b0b848521d580d33cca92151116
CarlosCaceres12 <carlosandres0913@gmail.com>
2025-09-26 11:11:31 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -7,22 +7,25 @@ menuToggle.addEventListener("click", () => {
 });
 
 // Scroll suave al hacer clic en los enlaces
-const links = document.querySelectorAll('.nav-links a');
+const links = document.querySelectorAll('.nav-links a, .btn');
 
 links.forEach(link => {
   link.addEventListener("click", (e) => {
-    e.preventDefault();
-    const targetId = link.getAttribute("href").substring(1);
-    const targetElement = document.getElementById(targetId);
+    if (link.getAttribute("href").startsWith("#")) {
+      e.preventDefault();
+      const targetId = link.getAttribute("href").substring(1);
+      const targetElement = document.getElementById(targetId);
 
-    if (targetElement) {
-      window.scrollTo({
-        top: targetElement.offsetTop - 70, // Ajusta por el header fijo
-        behavior: "smooth"
-      });
+      if (targetElement) {
+        const headerHeight = document.querySelector("header").offsetHeight;
+        window.scrollTo({
+          top: targetElement.offsetTop - headerHeight,
+          behavior: "smooth"
+        });
+      }
+
+      // Cierra el menú después de hacer clic (en móvil)
+      navLinks.classList.remove("active");
     }
-
-    // Cierra el menú después de hacer clic (en móvil)
-    navLinks.classList.remove("active");
   });
 });
